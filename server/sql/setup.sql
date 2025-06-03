cbee97538084afe44d7fa19c17edd12070777609
Anton Krasnov <krasnov.aa5801@gmail.com>
2025-06-03 22:49:39 +0300

    sql

--- server/sql/setup.sql
+++ server/sql/setup.sql
@@ -1,1 +1,18 @@
-select * from pg_stat_replication;
+CREATE DATABASE paint;
+
+CREATE SEQUENCE user_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+
+CREATE TABLE IF NOT EXISTS public."user"
+(
+    id integer NOT NULL DEFAULT nextval('user_id_seq'::regclass),
+    username character varying COLLATE pg_catalog."default" NOT NULL,
+    login character varying COLLATE pg_catalog."default" NOT NULL,
+    password character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT user_pkey PRIMARY KEY (id),
+    CONSTRAINT unigue_login UNIQUE (login)
+)
